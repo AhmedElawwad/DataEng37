ba3141abf42cbbde9b2074885f9c8cd1a1aec3a3
Ahmed <ed.elawwad@gmail.com>
2022-05-23 14:28:28 +0100

    added fizz buzz to github

--- control_flow/fizz_buzz.py
+++ control_flow/fizz_buzz.py
@@ -2,9 +2,9 @@ def fizz_buzz():
     for each_number in range(1, 101):
         string = ""
         if each_number % 3 == 0:
-            string = string + "Fizz"
+            string +=  "Fizz"
         if each_number % 5 == 0:
-            string = string + "Buzz"
+            string +=  "Buzz"
         if each_number % 5 != 0 and each_number % 3 != 0:
             string = string + str(each_number)
         print(string)
