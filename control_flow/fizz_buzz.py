785cfcc0911c853fc9205a9336aecf715434d834
Ahmed <ed.elawwad@gmail.com>
2022-05-23 16:47:57 +0100

    added fizz buzz with a while loop

--- control_flow/fizz_buzz.py
+++ control_flow/fizz_buzz.py
@@ -1,13 +1,37 @@
-def fizz_buzz():
-    for each_number in range(1, 101):
-        string = ""
-        if each_number % 3 == 0:
-            string +=  "Fizz"
-        if each_number % 5 == 0:
-            string +=  "Buzz"
-        if each_number % 5 != 0 and each_number % 3 != 0:
-            string = string + str(each_number)
-        print(string)
+# def fizz_buzz():
+#     for each_number in range(1, 101):
+#         string = ""
+#         if each_number % 3 == 0:
+#             string +=  "Fizz"
+#         if each_number % 5 == 0:
+#             string +=  "Buzz"
+#         if each_number % 5 != 0 and each_number % 3 != 0:
+#             string = string + str(each_number)
+#         print(string)
+#
+# fizz_buzz()
+#
 
-fizz_buzz()
 
+
+
+def fizz_buzz_while():
+
+    number = int(input("Please type a number"))
+    while number <101:
+        if number % 5 == 0 and number % 3 == 0:
+            print("FizzBuzz")
+            fizz_buzz_while()
+        elif number % 3 == 0:
+            print("Fizz")
+            fizz_buzz_while()
+        elif number % 5 == 0:
+            print("Buzz")
+            fizz_buzz_while()
+        else:
+            print (number)
+
+
+        number += 1
+
+fizz_buzz_while()
