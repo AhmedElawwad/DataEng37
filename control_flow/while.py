ebfada83d3427a5bac5b2405df1232228e8d4a52
Ahmed <ed.elawwad@gmail.com>
2022-05-23 08:21:11 +0100

    added fizz buzz to github

--- control_flow/while.py
+++ control_flow/while.py
@@ -1,11 +1,13 @@
-for number in range(1,101):
+def fizz_buzz():
+    for each_number in range(1, 101):
+        string = ""
+        if each_number % 3 == 0:
+            string = string + "Fizz"
+        if each_number % 5 == 0:
+            string = string + "Buzz"
+        if each_number % 5 != 0 and each_number % 3 != 0:
+            string = string + str(each_number)
+        print(string)
 
-for num in range(1, 21):
-    string = ""
-    if num % 3 == 0:
-        string = string + "Fizz"
-    if num % 5 == 0:
-        string = string + "Buzz"
-    if num % 5 != 0 and num % 3 != 0:
-        string = string + str(num)
-    print(string)
+fizz_buzz()
+
