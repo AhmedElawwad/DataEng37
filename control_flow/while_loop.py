b4f20c19fbb5883f9e1b82b2bc5a7333d7e738cd
AhmedElawwad <ed.elawwad@gmail.com>
2022-05-28 23:58:51 +0100

    added extra practice code

--- control_flow/while_loop.py
+++ control_flow/while_loop.py
@@ -49,3 +49,13 @@
 # while index < length:
 #     print(ingredients[index])
 #     index += 1
+
+
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+
+while index < length:
+    print("I am learning about", python_topics[index])
+    index += 1
