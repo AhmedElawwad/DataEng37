b678817046d85f5f186d44be47e3d62ab39720b7
AhmedElawwad <ed.elawwad@gmail.com>
2022-05-28 23:29:19 +0100

    added extra practice code

--- control_flow/while_loop.py
+++ control_flow/while_loop.py
@@ -1,17 +1,33 @@
-age = input("What is your age?\n")
+# age = input("What is your age?\n")
+#
+# keep_asking = True
+#
+# age_int = None
+#
+# while keep_asking:
+#     age = input("What is your age?\n")
+#     if age.isdigit():
+#         age_int = int(age)
+#         keep_asking = False
+#
+#     else:
+#         print("Please enter a valid number in digits.")
+#
+# print(f"Your age is {age_int}")
 
-keep_asking = True
 
-age_int = None
+# While Loop Walkthrough
+count = 0
+print("Starting While Loop")
+while count <= 3:
+    # Loop Body
+    # Print if the condition is still true
+    print("Loop Iteration - count <= 3 is still true")
+    # Print the current value of count
+    print("Count is currently " + str(count))
+    # Increment count
+    count += 1
+    print(" ----- ")
+print("While Loop ended")
 
-while keep_asking:
-    age = input("What is your age?\n")
-    if age.isdigit():
-        age_int = int(age)
-        keep_asking = False
-
-    else:
-        print("Please enter a valid number in digits.")
-
-print(f"Your age is {age_int}")
-
+# Your code below:
